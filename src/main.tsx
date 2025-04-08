9b1bfe2f2d0f78d66595afc4a768242b277280e4
Denis Tankov <104718229+DenisTankov@users.noreply.github.com>
2025-04-08 09:43:07 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globals.scss';
-import './styles/reset.scss';
+// import './styles/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
